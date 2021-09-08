0811927582367f6fc4bd6520807aaee44d6d71de
Rafael Nery Machado <rafaelnerymachado@gmail.com>
2021-09-08 16:21:46 -0300

    requisito 5 - apipi perguntas - concluido

--- src/pages/GameScreen/index.jsx
+++ src/pages/GameScreen/index.jsx
@@ -1,11 +1,47 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { fetchQuestions } from '../../redux/actions';
+import Question from '../../components/Question';
 
 class GameScreen extends React.Component {
+  componentDidMount() {
+    const { getQuestions } = this.props;
+    getQuestions();
+  }
+
   render() {
+    const { questions } = this.props;
+
     return (
-      <div>TEST</div>
+      <main>
+        {
+          questions.length && <Question { ...questions[0] } />
+        }
+      </main>
     );
   }
 }
 
-export default GameScreen;
+GameScreen.propTypes = {
+  getQuestions: PropTypes.func.isRequired,
+  questions: PropTypes.arrayOf(PropTypes.shape({
+    question: PropTypes.string,
+    category: PropTypes.string,
+  })),
+};
+
+GameScreen.defaultProps = {
+  questions: [],
+};
+
+const mapStateToProps = (state) => ({
+  questions: state.game.questions,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  getQuestions: () => dispatch(fetchQuestions()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(GameScreen);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -13,3 +13,19 @@ export const getTriviaToken = () => (dispatch) => {
       localStorage.setItem('token', token);
     });
 };
+
+export const SET_QUESTIONS = 'SET_QUESTIONS';
+
+const setQuestions = (questions) => ({
+  type: SET_QUESTIONS,
+  payload: questions,
+});
+
+export const fetchQuestions = () => async (dispatch, getState) => {
+  const { player: { token } } = getState();
+
+  const response = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`);
+  const { results } = await response.json();
+
+  dispatch(setQuestions(results));
+};
