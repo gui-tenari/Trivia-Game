c600fe1c4a2869fd428b82bf280dd370b0403026
Gui-Tenari <guilherme.tenarie@gmail.com>
2021-09-09 07:08:41 -0300

    Refatora cronometro = reset e stop

--- src/components/StopWatch/index.js
+++ src/components/StopWatch/index.js
@@ -7,31 +7,49 @@ class StopWatch extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      isStopped: false,
       timer: 30,
     };
 
     this.timer = this.timer.bind(this);
+    this.resetTimer = this.resetTimer.bind(this);
   }
 
   componentDidMount() {
-    const { timer } = this.state;
-    if (timer > 0) {
-      this.timer();
+    this.timer();
+  }
+
+  componentDidUpdate() {
+    const countDown = 29;
+    const { timer, isStopped } = this.state;
+    const { selected } = this.props;
+    if (isStopped && timer < countDown && !selected) {
+      this.resetTimer();
+    // } else if (!selected && timer === countDown) {
+    //   this.timer();
     }
+  }
+
+  resetTimer() {
+    this.setState({ timer: 30 });
+    this.timer()
   }
 
   timer() {
     const { answered } = this.props;
     const ONE_SECOND = 1000;
     const Id = setInterval(() => {
-      const { timer } = this.state;
-      if (timer > 0) {
+      const { timer  } = this.state;
+      const { selected } = this.props;
+      if (!selected && timer > 0) {
         this.setState((previous) => ({
           timer: previous.timer - 1,
+          isStopped: false,
         }));
       } else {
         answered(true);
         clearInterval(Id);
+        this.setState({ isStopped: true });
       }
     }, ONE_SECOND);
   }
@@ -46,10 +64,15 @@ class StopWatch extends Component {
 
 StopWatch.propTypes = {
   answered: PropTypes.func.isRequired,
+  selected: PropTypes.bool.isRequired,
 };
+
+const mapStateToProps = (state) => ({
+  selected: state.game.isAnswered,
+});
 
 const mapDispatchToProps = (dispatch) => ({
   answered: (payload) => dispatch(answerQuestion(payload)),
 });
 
-export default connect(null, mapDispatchToProps)(StopWatch);
+export default connect(mapStateToProps, mapDispatchToProps)(StopWatch);
